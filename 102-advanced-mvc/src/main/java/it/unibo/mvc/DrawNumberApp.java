9680f47a88b5b5722681e743cebc3b294a32763e
Francesco Pazzaglia <francesco.pazzaglia2@gmail.com>
2023-11-28 23:10:17 +0100

    Exercise nr. 102: config file done.

--- 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberApp.java
+++ 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberApp.java
@@ -1,15 +1,31 @@
 package it.unibo.mvc;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.nio.file.Files;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
+
+import javax.swing.JOptionPane;
+import javax.xml.stream.util.StreamReaderDelegate;
 
 /**
  */
 public final class DrawNumberApp implements DrawNumberViewObserver {
-    private static final int MIN = 0;
-    private static final int MAX = 100;
-    private static final int ATTEMPTS = 10;
+    private static int MIN;
+    private static int MAX;
+    private static int ATTEMPTS;
+    private static final int CONFIG_PARAM = 3;
+    private static final int INDEX_VALUE_CONFIG = 1;
 
     private final DrawNumber model;
     private final List<DrawNumberView> views;
@@ -27,7 +43,17 @@ public final class DrawNumberApp implements DrawNumberViewObserver {
             view.setObserver(this);
             view.start();
         }
-        this.model = new DrawNumberImpl(MIN, MAX, ATTEMPTS);
+        final String PATH = System.getProperty("user.dir") +  "\\src\\main\\resources\\config.yml";
+        List<String> parameters = new LinkedList<>();
+        try (final BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(PATH)))) {
+            String line = "";
+            while ( (line = br.readLine()) != null){
+                parameters.add(line.split(" ")[INDEX_VALUE_CONFIG]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace(); 
+        }
+        this.model = new DrawNumberImpl(Integer.parseInt(parameters.get(0)), Integer.parseInt(parameters.get(1)), Integer.parseInt(parameters.get(2)));
     }
 
     @Override
